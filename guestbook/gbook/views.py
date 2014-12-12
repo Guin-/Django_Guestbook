5efecfa6106530f0f942dd6c9c90e869a66ad626
Angelika Jarosz <angelikajarosz@gmail.com>
2014-12-12 17:43:55 -0500

    update views and index, empty form now displays

--- guestbook/gbook/views.py
+++ guestbook/gbook/views.py
@@ -1,28 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from gbook.forms import EntryForm
 
 def index(request):
 	if request.method == "POST":
-		form = EntryForm(request.POST)
 		if form.is_valid():
-			#model_instance = form.save(commit = False)
-			#model_instance.timestamp = timezone.now()
-			#model_instance.save()
-			return redirect('success')
-		else:
-			form = EntryForm()
+			form = EntryForm(request.POST)
+		
+		#model_instance = form.save(commit = False)
+		#model_instance.timestamp = timezone.now()
+		#model_instance.save()
+		return render(request, '/gbook/index.html', {'form':form})
+	else:
+		form = EntryForm()
 		return render(request, 'gbook/index.html', {'form':form})
 
-'''
-from django import forms
-from django.forms import ModelForm
-from gbook import models as m
-from gbook import forms
-from gbook.forms import EntryForm
-'''
 
 
+
+
+
+
+# return form data on same index.html template below the display of the form
 
 '''def index(request):
 	return HttpResponse("Hello Guins, you are at the Guin Guestbook Index")'''
