f66b2fe9afb6e0aa0c5e39b1d57a8f1e63582ea1
Angelika Jarosz <angelikajarosz@gmail.com>
2014-12-12 20:18:10 -0500

    add base css file

--- guestbook/gbook/views.py
+++ guestbook/gbook/views.py
@@ -2,23 +2,39 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from gbook.forms import EntryForm
 
+import datetime  
+
+
+
 def index(request):
 	if request.method == "POST":
+		form = EntryForm(request.POST)
 		if form.is_valid():
-			form = EntryForm(request.POST)
-		
-		#model_instance = form.save(commit = False)
-		#model_instance.timestamp = timezone.now()
-		#model_instance.save()
-		return render(request, '/gbook/index.html', {'form':form})
+			f_instance = form.save()
+			#model_instance.timestamp = timestamp
+			#model_instance.save()
+		return render(request, 'gbook/index.html', {'form':form})
 	else:
 		form = EntryForm()
 		return render(request, 'gbook/index.html', {'form':form})
 
+def entry(request):
+	if request.method == "POST" and form.is_valid():
+		f_instance = form.save()
+		return f_instance
 
+# Form shows up. Fill out. Submit. Form returned with fields filled in. 	
+'''
+def index(request):
+	if request.method == "POST":
+		form = EntryForm(request.POST)
+		if form.is_valid():
+			#f_instance = form.save()
+			#model_instance.timestamp = timestamp
+			#model_instance.save()
+			return render(request, 'gbook/index.html', {'form':form})
 
-
-
+'''
 
 
 # return form data on same index.html template below the display of the form
